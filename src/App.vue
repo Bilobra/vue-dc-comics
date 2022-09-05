287e91348b2bc1ff587a40eaf3556e061e0b0ea9
Bilobra <bra.carrozzo@gmail.com>
2022-09-05 16:30:00 +0200

    add blue section content

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
    <MainHeader></MainHeader>
    <MainContent></MainContent>
+   <SecondContent></SecondContent>
   </div>
 </template>
 
 <script>
 import MainHeader from './components/MainHeader.vue'
 import MainContent from './components/MainContent.vue'
+import SecondContent from './components/SecondContent.vue'
 
 export default {
   name: 'App',
   components:{
     MainHeader,
     MainContent,
+    SecondContent,
   },
  
 }
---
+++ src/components/SecondContent.vue
@@ -0,0 +1,112 @@
+<template>
+    <section class="light_section">
+        <div class="container">
+            <ul>
+                <li class="content-link">
+
+                    <figure class="img_content">
+                        <img src="../assets/buy-comics-digital-comics.png" alt="digitalComics">
+                    </figure>
+                    <div>
+                        <a href="#">
+                            digital comics
+                        </a>
+
+                    </div>
+
+                </li>
+                <li class="content-link">
+
+                    <figure class="img_content">
+                        <img src="../assets/buy-comics-merchandise.png" alt="digitalComics">
+                    </figure>
+                    <div>
+                        <a href="#">
+                            dc merchandise
+                        </a>
+
+                    </div>
+
+                </li>
+                <li class="content-link">
+
+                    <figure class="img_content">
+                        <img src="../assets/buy-comics-subscriptions.png" alt="digitalComics">
+                    </figure>
+                    <div>
+                        <a href="#">
+                            subscription
+                        </a>
+
+                    </div>
+
+                </li>
+                <li class="content-link">
+
+                    <figure class="img_content">
+                        <img src="../assets/buy-comics-shop-locator.png" alt="digitalComics">
+                    </figure>
+                    <div>
+                        <a href="#">
+                            comic shop locator
+                        </a>
+
+                    </div>
+
+                </li>
+                <li class="content-link">
+
+                    <figure class="img_content">
+                        <img src="../assets/buy-dc-power-visa.svg" alt="digitalComics">
+                    </figure>
+                    <div>
+                        <a href="#">
+                            comic shop locator
+                        </a>
+
+                    </div>
+
+                </li>
+
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables';
+
+.light_section {
+    background-color: $light-blue;
+    padding: 75px 0;
+    color: white;
+
+    ul {
+        display: flex;
+        align-items: center;
+        
+        gap: 25px;
+
+        .content-link {
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+            text-transform: uppercase;
+            gap: 20px;
+            
+
+            .img_content {
+                width: 50px;
+                height: 60px;
+                
+            }
+        }
+    }
+}
+</style>
