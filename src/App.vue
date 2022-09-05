c660a68bc6fb77582fce3149b66350337741e239
Bilobra <bra.carrozzo@gmail.com>
2022-09-05 17:48:58 +0200

    add footerOne section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-   <MainHeader></MainHeader>
-   <MainContent></MainContent>
-   <SecondContent></SecondContent>
-   <MainFooter></MainFooter>
+    <MainHeader></MainHeader>
+    <MainContent></MainContent>
+    <SecondContent></SecondContent>
+    <MainFooter></MainFooter>
+    <FooterOne></FooterOne>
   </div>
 </template>
 
@@ -12,19 +13,23 @@ import MainHeader from './components/MainHeader.vue'
 import MainContent from './components/MainContent.vue'
 import SecondContent from './components/SecondContent.vue'
 import MainFooter from './components/MainFooter.vue'
+import FooterOne from './components/FooterOne.vue'
+
 
 export default {
   name: 'App',
-  components:{
+  components: {
     MainHeader,
     MainContent,
     SecondContent,
     MainFooter,
-  },
- 
+    FooterOne,
+   
+},
+
 }
 </script>
 
 <style lang="scss">
-  @import './style/index.scss';
+@import './style/index.scss';
 </style>
---
+++ src/components/FooterOne.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="footer">
+        .<div class="container">
+            <div class="row">
+                <div>
+                    <button class="button_col">sign-up now!</button>
+                </div>
+                <div>
+                    <ul>
+                        <li class="follow_content">
+                            <a href="#">
+                                Follow us
+                            </a>
+                        </li>
+                        <li class="icon-content">
+                            <a href="#">
+                                <img src="../assets/footer-twitter.png" alt="fb">
+                            </a>
+                        </li>
+                        <li class="icon-content">
+                            <a href="#">
+                                <img src="../assets/footer-youtube.png" alt="fb">
+                            </a>
+                        </li>
+                        <li class="icon-content">
+                            <a href="#">
+                                <img src="../assets/footer-pinterest.png" alt="fb">
+                            </a>
+                        </li>
+                        <li class="icon-content">
+                            <a href="#">
+                                <img src="../assets/footer-periscope.png" alt="fb">
+                            </a>
+                        </li>
+                        <li class="icon-content">
+                            <a href="#">
+                                <img src="../assets/footer-facebook.png" alt="fb">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style lang="scss">
+    
+</style>
